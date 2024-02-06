4500cef04d0e80fcf8c7430b85226a38f60fce7a
dato201 <146935809+dato201@users.noreply.github.com>
2024-02-06 16:57:15 +0400

    all lesson 2 finished

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,39 @@ import './App.css'
 
 
 
-const App = ({name, lastName, age, id}) => {
+const App = ({ children}) => {
   // const [count, setCount] = useState(0)
 
+  let users = [
+    { name: "dato", lastName: "balxamishvili", age: 19, id: "998737473", id2: Math.floor(Math.random() * 1000)},
+    { name: "guram", lastName: "otarashvili", age: 40, id: "354869498", id2: Math.floor(Math.random() * 1000)},
+    { name: "soso", lastName: "ipsumoshvili", age: 20, id: "3534644869498", id2: Math.floor(Math.random() * 1000)},
+  ];
+
+  let userLogin = false
+
+  if (users[0] === undefined) {
+  userLogin = true
+  }
   return (
     <>
+    {children}
+      <div className='user_div'>
+        {users.map(user => {
+          return (
+            <button key={user.id2}>
+              <p key={user.name}><span className='span_user'>name: </span>  {user.name}</p>
+              <p key={user.lastName}><span className='span_user'>last name: </span>  {user.lastName}</p>
+              <p key={user.age}><span className='span_user'>age: </span> {user.age}</p>
+              <p key={user.id}><span className='span_user'>id: </span>  {user.id}</p>
+            </button>
+          )
+        })}
+        {userLogin && (
+          <h1 className='users_system'>no users in the system.</h1>
+        )}
+      </div>
 
-      <button>
-        <p><span className='span_user'>name: </span>  {name}</p>
-        <p><span className='span_user'>last name: </span>  {lastName}</p>
-        <p><span className='span_user'>age: </span> {age}</p>
-        <p><span className='span_user'>id: </span>  {id}</p>
-      </button>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,24 +2,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-let users = [
-  { name: "dato", lastName: "balxamishvili", age: 19, id: "839583909030" },
-  { name: "guram", lastName: "otarashvili", age: 40, id: "112293870095" },
-  { name: "givi", lastName: "ipsumishvili", age: 27, id: "009913772763" },
-  { name: "tato", lastName: "givishvili", age: 12, id: "550099392039" },
-  { name: "lorem", lastName: "lorem ipsum", age: 39, id: "992388739374" },
-]
-
 ReactDOM.createRoot(document.getElementById('root')).render(
-
-  <div className='user_div'>
-    <App name={users[0].name} lastName={users[0].lastName} age={users[0].age} id={users[0].id} />
-    <App name={users[1].name} lastName={users[1].lastName} age={users[1].age} id={users[1].id} />
-    <App name={users[2].name} lastName={users[2].lastName} age={users[2].age} id={users[2].id} />
-    <App name={users[3].name} lastName={users[3].lastName} age={users[3].age} id={users[3].id} />
-    <App name={users[4].name} lastName={users[4].lastName} age={users[4].age} id={users[4].id} />
-  </div>
+    <App />
 )
-
-
-//   </React.StrictMode>,<React.StrictMode>
